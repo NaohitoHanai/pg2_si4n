4181eac3cef4f3a7def679ec84e67c92f09249c2
hanamaruGames <nao@hanamaru.games>
2024-01-10 16:40:17 +0900

    1/10授業開始

---
+++ SwordBout/project/Source/Player.cpp
@@ -0,0 +1,23 @@
+#include "Player.h"
+#include "Stage.h"
+#include "../Library/Time.h"
+
+Player::Player()
+{
+}
+
+Player::~Player()
+{
+}
+
+void Player::Start()
+{
+}
+
+void Player::Update()
+{
+}
+
+void Player::Draw()
+{
+}
