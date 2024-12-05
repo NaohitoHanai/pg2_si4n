540689443f07ddc78d79958b47aebaf4fc436211
花井直人 <nao@hanamaru.games>
2024-12-05 16:36:00 +0900

    軌跡を入れました

--- SwordBout/project/Source/Locus.h
+++ SwordBout/project/Source/Locus.h
@@ -17,4 +17,15 @@ public:
 	/// <param name="root">柄の座標</param>
 	/// <param name="tip">剣先の座標</param>
 	void Add(VECTOR root, VECTOR tip);
+private:
+	static const int KeepNum = 20;
+	struct Position {
+		VECTOR root;
+		VECTOR tip;
+	};
+	Position positions[KeepNum];
+	int writeP;  // 次に書く場所
+	int oldestP; // 一番古い場所
+
+	int hImage; // 画像ファイル
 };
